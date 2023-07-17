7746e9323c3e4b5233f328b5272b662e75d44803
AAndrews-1982 <altonandrews@icloud.com>
2023-07-17 12:06:37 -0700

    made a change for web app and port

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+""" A simple flask web application that runs at 0.0.0.0:5000 """
+
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
-app.debug = True
-
-
-def StartFlask():
-    """ Start a Flask web application """
-    app.run(host='0.0.0.0', port=5000)
-
-
-@app.route('/states_list')
-def states_list():
-    """Display a HTML page with a list of all states"""
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
-
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+def teardown_session(exception):
+    """ Teardown session """
     storage.close()
 
 
-if __name__ == "__main__":
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    s_list = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                           Table="States",
+                           items=s_list)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
