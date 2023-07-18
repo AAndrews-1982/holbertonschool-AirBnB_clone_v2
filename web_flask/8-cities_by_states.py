98d2fcce2b2defb326c03811795563d2a686bfd0
AAndrews-1982 <altonandrews@icloud.com>
2023-07-18 07:55:18 -0700

    cites and states update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,8 +13,8 @@ def teardown_appcontext(exception):
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Display HTML page with States and their cities """
+def states_list():
+    """ Display HTML page with States and their Cities """
     states = storage.all('State').values()
     states = sorted(states, key=lambda x: x.name)
     return render_template('8-cities_by_states.html', states=states)
