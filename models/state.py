4b1044200327cd2187f24d72a51297d0a8b61675
AAndrews-1982 <altonandrews@icloud.com>
2023-07-18 07:30:00 -0700

    changes made to file storage and state files

--- models/state.py
+++ models/state.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.engine.file_storage import FileStorage
 from models.city import City
-import os
+from os import getenv
 import models
-
-if os.getenv("HBNB_TYPE_STORAGE") == 'db':
 
 
 class State(BaseModel, Base):
     """ State class """
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
-
-        class State(BaseModel):
-            """ File Storage """
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
-        
         def cities(self):
-            """Returns a list of City instances with state_id = id"""
-            cities = []
-            for thing in models.storage.all("City").values():
-                if thing.state_id == self.id:
-                    cities.append(thing)
-            return cities
+            """placeholder"""
+            city_list = []
+            for el in models.storage.all(City).values():
+                if el.state_id == self.id:
+                    city_list.append(el)
+            return city_list
