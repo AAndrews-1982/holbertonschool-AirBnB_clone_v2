46d55b9aa204d9e1672c69885a35a772edf58ebd
AAndrews-1982 <altonandrews@icloud.com>
2023-07-18 19:20:08 -0700

    changes made to 8-cities and 8-cities_html

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,20 +4,22 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """tears down session"""
-    storage.close()
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """ Display HTML page with States and their Cities """
+@app.route('/cities_by_states')
+def cities_by_states():
+    """ Display HTML page with States and their cities """
     states = storage.all('State').values()
     states = sorted(states, key=lambda x: x.name)
     return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
